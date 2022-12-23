e1a7321fdcbe200c2bf1dab2ff649994e2b43394
Arthur Loefstedt <theloefstedtab@gmail.com>
2022-12-23 14:06:35 -0500

    async project

---
+++ newdata.js
@@ -0,0 +1,70 @@
+const ANA = { xGoals: 0.46, Corsi: 0.47, Fenwick: 0.47 };
+const ARI = { xGoals: 0.42, Corsi: 0.43, Fenwick: 0.43 };
+const BOS = { xGoals: 0.54, Corsi: 0.53, Fenwick: 0.55 };
+const BUF = { xGoals: 0.45, Corsi: 0.48, Fenwick: 0.47 };
+const CAR = { xGoals: 0.56, Corsi: 0.56, Fenwick: 0.55 };
+const CBJ = { xGoals: 0.45, Corsi: 0.48, Fenwick: 0.47 };
+const CGY = { xGoals: 0.56, Corsi: 0.55, Fenwick: 0.55 };
+const CHI = { xGoals: 0.47, Corsi: 0.46, Fenwick: 0.46 };
+const COL = { xGoals: 0.53, Corsi: 0.53, Fenwick: 0.52 };
+const DAL = { xGoals: 0.53, Corsi: 0.5, Fenwick: 0.5 };
+const DET = { xGoals: 0.45, Corsi: 0.47, Fenwick: 0.46 };
+const EDM = { xGoals: 0.54, Corsi: 0.52, Fenwick: 0.51 };
+const FLA = { xGoals: 0.55, Corsi: 0.56, Fenwick: 0.55 };
+const LAK = { xGoals: 0.53, Corsi: 0.54, Fenwick: 0.55 };
+const MIN = { xGoals: 0.51, Corsi: 0.51, Fenwick: 0.51 };
+const MTL = { xGoals: 0.44, Corsi: 0.47, Fenwick: 0.46 };
+const NJD = { xGoals: 0.5, Corsi: 0.51, Fenwick: 0.5 };
+const NSH = { xGoals: 0.49, Corsi: 0.49, Fenwick: 0.49 };
+const NYI = { xGoals: 0.48, Corsi: 0.47, Fenwick: 0.47 };
+const NYR = { xGoals: 0.51, Corsi: 0.48, Fenwick: 0.48 };
+const OTT = { xGoals: 0.48, Corsi: 0.48, Fenwick: 0.48 };
+const PHI = { xGoals: 0.46, Corsi: 0.47, Fenwick: 0.47 };
+const PIT = { xGoals: 0.53, Corsi: 0.52, Fenwick: 0.53 };
+const SEA = { xGoals: 0.47, Corsi: 0.51, Fenwick: 0.5 };
+const SJS = { xGoals: 0.46, Corsi: 0.46, Fenwick: 0.47 };
+const STL = { xGoals: 0.5, Corsi: 0.48, Fenwick: 0.48 };
+const TBL = { xGoals: 0.52, Corsi: 0.51, Fenwick: 0.51 };
+const TOR = { xGoals: 0.56, Corsi: 0.54, Fenwick: 0.54 };
+const VAN = { xGoals: 0.5, Corsi: 0.5, Fenwick: 0.5 };
+const VGK = { xGoals: 0.51, Corsi: 0.52, Fenwick: 0.53 };
+const WPG = { xGoals: 0.49, Corsi: 0.5, Fenwick: 0.5 };
+const WSH = { xGoals: 0.52, Corsi: 0.51, Fenwick: 0.52 };
+
+
+
+
+module.exports = {
+  ANA,
+ARI,
+BOS,
+BUF,
+CAR,
+CBJ,
+CGY,
+CHI,
+COL,
+DAL,
+DET,
+EDM,
+FLA,
+LAK,
+MIN,
+MTL,
+NJD,
+NSH,
+NYI,
+NYR,
+OTT,
+PHI,
+PIT,
+SEA,
+SJS,
+STL,
+TBL,
+TOR,
+VAN,
+VGK,
+WPG,
+WSH,
+}
